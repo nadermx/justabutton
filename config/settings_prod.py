b67e68ecce59b44ed380a872976df4eeea40a471
john <john@nader.mx>
2025-10-31 19:19:48 -0600

    Fix HTTPS redirect loop
    - Add SECURE_PROXY_SSL_HEADER to settings_prod.py
    - Add X-Forwarded-Proto header to nginx config

--- config/settings_prod.py
+++ config/settings_prod.py
@@ -7,6 +7,7 @@ ALLOWED_HOSTS = ['justabutton.org', 'www.justabutton.org', '137.184.12.133']
 
 # Security settings
 SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_BROWSER_XSS_FILTER = True
